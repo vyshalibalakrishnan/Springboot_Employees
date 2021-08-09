648a29de3e744e33dfa765ab5b6fcfe55dc8a966
vyshalibalakrishnan <61009839+vyshalibalakrishnan@users.noreply.github.com>
2021-08-09 20:35:13 +0530

    Update EmployeeController.java

--- src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -20,7 +20,7 @@ import net.javaguides.springboot.exception.ResourceNotFoundException;
 import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://34.93.225.134:8090")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
